02f074a614b7d1a261d603fb533c6bc29bccc468
jokerrobert920 <jokerrobert920@gmail.com>
2022-06-10 18:34:31 -0700

    A framework for building public blockchains

---
+++ x/checkers/genesis_test.go
@@ -0,0 +1,23 @@
+package checkers_test
+
+import (
+	"testing"
+
+	keepertest "github.com/alice/checkers/testutil/keeper"
+	"github.com/alice/checkers/x/checkers"
+	"github.com/alice/checkers/x/checkers/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenesis(t *testing.T) {
+	genesisState := types.GenesisState{
+		// this line is used by starport scaffolding # genesis/test/state
+	}
+
+	k, ctx := keepertest.CheckersKeeper(t)
+	checkers.InitGenesis(ctx, *k, genesisState)
+	got := checkers.ExportGenesis(ctx, *k)
+	require.NotNil(t, got)
+
+	// this line is used by starport scaffolding # genesis/test/assert
+}
